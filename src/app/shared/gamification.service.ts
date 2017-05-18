b0325c41b9f2450848b48d44fb5de7c30a76862c
André Nunes <dekonunesss@gmail.com>
2017-05-18 02:37:13 -0300

    Update Gamification

--- src/app/shared/gamification.service.ts
+++ src/app/shared/gamification.service.ts
@@ -6,21 +6,15 @@ export class GamificationService {
 
   private gamification: Gamification[] = [
     new Gamification (0, true, "Ranking", 0, 'Ganhe pontos'),
-    // new Gamification (1, true, "[Muito Mal, Mal, Indiferente, Bem, Muito Bem]", 'Amigos', "Como você se sente hoje no relacionamento com seus amigos?"),
-    // new Gamification (2, true, "[Muito Mal, Mal, Indiferente, Bem, Muito Bem]", 'Colegas de trabalho', "Como você se sente hoje no relacionamento com seus colegas de trabalho?"),
-    // new Gamification (3, true, "[Muito Preconceito, Bastante, Neutro , Pouco, Nenhum Preconceito]", 'Preconceito', "Como você se sente hoje com relação ao preconceito que tem sofrido?"),
-    // new Gamification (4, true, "[Muita Fissura, Bastente, Neutro, Pouca, Nenhuma Fissura]", "Vontade de uso", "Como você se sente hoje com relação as “fissuras” ao longo do dia?" ),
-    // new Gamification (5, true, "[Muita Mudança, Bastante , Neutro , Pouca, Nenhuma Mudança]", 'Humor', "Como você se sente hoje com relação as possíveis mudanças no seu humor ao longo do dia?"),
-    // new Question (6, true, "[Nunca, Esporádico, Ocasional, Pouco frequente, Frequente]", "Contato com a CT", "Quão frequente tem sido o seu contato com a CT onde esteve acolhido?"),
-    // new Question (7, true, "[Nunca, Esporádico, Ocasional, Pouco frequente, Frequente]", "Contato com grupos de apoio", "Quão frequente tem sido o seu contato com entidades que auxiliam namanutenção?"),
-    // new Question (8, true, "[Nunca, Esporádico, Ocasional, Pouco frequente, Frequente]", 'Espiritualidade', "Quão frequente tem sido a sua preocupação com sua espiritualidade?"),
-    // new Question (9, true, "[Muito Preconceito, Bastante, Neutro , Pouco, Nenhum Preconceito]", 'Moradia', 'Como você se sente hoje no relacionamento com seus familiares?'),
-    // new Question (10, true, "[Muito Preconceito, Bastante, Neutro , Pouco, Nenhum Preconceito]", 'Emprego', 'Como você se sente hoje no relacionamento com seus familiares?'),
-    // new Question (11, true, "[Muito Preconceito, Bastante, Neutro , Pouco, Nenhum Preconceito]", 'Educação', 'Como você se sente hoje no relacionamento com seus familiares?'),
-    // new Question (12, true, "[Muito Preconceito, Bastante, Neutro , Pouco, Nenhum Preconceito]", 'Lazer', 'Como você se sente hoje no relacionamento com seus familiares?'),
-    // new Question (13, true, "[Muito Preconceito, Bastante, Neutro , Pouco, Nenhum Preconceito]", 'Esporte', 'Como você se sente hoje no relacionamento com seus familiares?'),
-    // new Question (14, true, "[Abandonei, Tomo quando acho que preciso, Tomo de vez em quando, Tomo quando lembro, Tomo Regularmente]", 'Medicação', 'Como você se sente hoje no relacionamento com seus familiares?')
-  ];
+    new Gamification (1, true, "Série C", 0, 'Responda por 2 dias consecutivos'),
+    new Gamification (2, true, "Série B", 0, 'Responda por 7 dias consecutivos'),
+    new Gamification (3, true, "Série A", 0, 'Responda por 30 dias consecutivos'),
+    new Gamification (4, true, "Base", 0, 'Responda por 7 dias todas as perguntas'),
+    new Gamification (5, true, "Reserva", 0, 'Responda por 15 dias todas as perguntas'),
+    new Gamification (6, true, "Titular", 0, 'Responda por 30 dias todas as perguntas'),
+    new Gamification (7, true, "Time", 0, 'Dias respondendo consectivamente'),
+    new Gamification (8, true, "Rodada", 0, 'Semanas respondendo consectivamente')
+    ];
 
   constructor() {}
 
